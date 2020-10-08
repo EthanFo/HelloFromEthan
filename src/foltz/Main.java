5203f21c73f4d2c99fe761c2579022f4343ebcc1
Eli-B-Washington <bea14018@byui.edu>
2020-10-08 10:44:03 -0700

    commit message

--- src/foltz/Main.java
+++ src/foltz/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Ethan.");
-        System.out.println("Ethan is the best.");
+        System.out.println("Hello Ethan, My Name is Eli.");
     }
 }
